ab2e4839d1d8a1f4e7682e88e0080733a9f45fe3
patilsushama021 <patilsushama021@gmail.com>
2023-02-16 20:42:07 +0530

    JS Assignment-01
    JS Assignment-01 on variable

---
+++ 01variableAndDatatypes/variableAssignment.js
@@ -0,0 +1,26 @@
+var myFullName;   // variable declaration
+myFullName="Sushama Patil";//variable initialization
+console.log("My Full Name:", myFullName);
+myFullName="Sushama Sunil Patil";
+console.log("My Full Name:", myFullName);
+console.log('**************01**************');
+var myLove;
+myLove="I love only JS";
+console.log("My Love Value:",myLove);
+console.log('***********','02','***********');
+var firstName
+var lastName
+var age
+var address
+var pincode
+var gender
+var ismarried
+firstName="Sushama"
+lastName="Patil"
+age="27"
+address="Pune"
+pincode="411019"
+gender="Female"
+ismarried="Single"
+console.log("Name:",firstName, lastName, "\nAge:",age, "\nAddress:",address, pincode, "\nGender:",gender, "\nismarried:",ismarried)
+
