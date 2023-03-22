19825445e0c17e877d4fd67fd902d066f8ad2db5
patilsushama021 <patilsushama021@gmail.com>
2023-03-22 17:44:53 +0530

    JS Assignment 19
    map on object

--- 16_mapMethodAssign02.js
+++ 16_mapMethodAssign02.js
@@ -15,5 +15,30 @@ const emp_monika = new Employee(77, "Monika", "IT", 40000, "Wipro");
 const emp_viny = new Employee(88, "Vinayak", "IT", 75000, "TCS");
 const emp_mahi = new Employee(99, "Mahesh", "HR", 85000, "Infy");
 
-let mapEmployees = [emp_anil, emp_radha, emp_rishi, emp_sonali, emp_monika, emp_mahi]
-mapEmployees.set(99, emp_mahi);
+let mapEmployees = [emp_anil, emp_radha, emp_rishi, emp_sonali, emp_monika, emp_viny, emp_mahi];
+
+// 1. Get the list of all employee names & log new array on console.
+
+let empNames = mapEmployees.map( empDetail => empDetail.emp_name);
+console.log(`List of employee name: ${empNames}`);
+console.log(`***************************************************`);
+
+// 2. Get the list of departments & log on console
+let dept = mapEmployees.map(empDetail => empDetail.emp_dept);
+console.log(`List of employee Departments: ${dept}`);
+console.log(`***************************************************`);
+
+// 3. Get the list of employee id's and log on console
+let id = mapEmployees.map(empDetail => empDetail.emp_id);
+console.log(`List of employee Id: ${id}`);
+console.log(`**************************************************`)
+
+// 4. Get the list employee names working in TCS
+let sameCompanyWorking = mapEmployees.map(empDetail =>{
+    if(empDetail.emp_company =="TCS"){
+        return empDetail.emp_name;
+    }
+});
+console.log(`List of employee names working in TCS: ${sameCompanyWorking}`);
+
+
